dc1d5dfbe904fb8e70f156f0625fc9c61e3b83ec
debibl <miryan.vasilina@gmail.com>
2024-04-20 21:57:52 +0300

    [STEP 2] add a format selection flag

--- gendiff.js
+++ gendiff.js
@@ -1,13 +1,18 @@
+#!/usr/bin/env node
 import { program } from 'commander';
 
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .option('-h, --help', 'output usage information')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2, cmd) => {
+    console.log('filepath1:', filepath1);
+    console.log('filepath2:', filepath2);
+    console.log('format:', cmd.format || 'default');
+  })
   .parse(process.argv);
 
-if (program.help) {
+if (!process.argv.slice(2).length) {
   program.outputHelp();
-} else {
-  console.log('Usage: gendiff [options]');
 }
